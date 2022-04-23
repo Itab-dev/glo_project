d3c2fe04e1900ce5abc6aa407a3222ca8d428ce7
Ivan Taboyakov <taboyakov.ivan@mail.ru>
2022-04-23 15:55:31 +0300

    Add scrolls

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+const modal = document.querySelector('.modal');
+const modalButton = document.querySelector('.modal__button');
+const courseButton = document.querySelector('.course__button');
+function modalAppend(){
+    modal.style.display = 'flex';
+}
+courseButton.addEventListener('click',modalAppend);
+modalButton.addEventListener('click',modalAppend);
+
+modal.addEventListener('click',(event)=>{
+    const modalContent = event.target;
+    if(modalContent.classList.contains('modal')||modalContent.classList.contains('closeBtn')){
+        modal.style.display = '';
+    }
+})
---
+++ js/smoothScroll.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('.header__nav');
+const links = nav.querySelectorAll('a')
+links.forEach(link => {
+    link.addEventListener('click',(event)=>{
+        event.preventDefault();
+        const section = document.querySelector(link.getAttribute('href'))
+        if(section){
+            seamless.scrollIntoView(section, {
+                behavior: "smooth",
+                block: "start",
+                inline: "center",
+            });
+        }
+    })
+})
